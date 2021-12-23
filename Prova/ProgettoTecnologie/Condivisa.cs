5b20eb95bb7b46ab68b034d9d12c3612e591f9d9
TeoFri <82151218+TeoFri@users.noreply.github.com>
2021-12-23 16:38:24 +0100

    Add files via upload
    C'è un errore, ho chiesto al prof come risolverlo

---
+++ Prova/ProgettoTecnologie/Condivisa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoTecnologie
+{
+    class Condivisa
+    {
+        string ipAdd;
+        string nickname;
+                
+        public void getIp(string ip)
+        {
+            ipAdd = ip;
+        }
+        public void getNick(string user)
+        {
+            nickname = user;
+        }
+
+        public string setIp()
+        {
+            return ipAdd;
+        }
+        public string setNick()
+        {
+            return nickname;
+        }
+    }
+}
