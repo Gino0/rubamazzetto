a0baa8085b8732e242bfccf0d3ab88b2907fbcc1
TeoFri <82151218+TeoFri@users.noreply.github.com>
2021-12-27 12:51:22 +0100

    Add files via upload

---
+++ Prova/ProgettoTecnologie/Invito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoTecnologie
+{
+    class Invito
+    {
+    
+        string nicknameAvv;
+
+        public void getNick(string user)
+        {
+            nicknameAvv = user;
+        }
+
+   
+        public string setNick()
+        {
+            return nicknameAvv;
+        }
+    }
+}
